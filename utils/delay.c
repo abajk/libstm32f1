bf306d81498d039cc993b7250f206ded713097b0
Aleksander Jan Bajkowski <olek2@wp.pl>
2019-05-23 20:41:21 +0200

    add delay

---
+++ utils/delay.c
@@ -0,0 +1,21 @@
+#include "stm32f10x.h"
+#include "delay.h"
+
+volatile uint32_t msTicks;                                 // counts 1ms timeTicks
+
+/*----------------------------------------------------------------------------
+ * SysTick_Handler:
+ *----------------------------------------------------------------------------*/
+void SysTick_Handler(void) {
+  msTicks++;
+}
+
+/*----------------------------------------------------------------------------
+ * Delay: delays a number of Systicks
+ *----------------------------------------------------------------------------*/
+extern void delay (uint32_t dlyTicks) {
+  uint32_t curTicks;
+
+  curTicks = msTicks;
+  while ((msTicks - curTicks) < dlyTicks) { __NOP(); }
+}
